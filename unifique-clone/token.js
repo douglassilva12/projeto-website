2b65fa426047b77bcfe7b0b913b32b4724f66134
Douglas <douglas.silva12@alunos.sc.senac.br>
2022-11-09 16:35:02 -0300

    ok

--- unifique-clone/token.js
+++ unifique-clone/token.js
@@ -1,7 +1,7 @@
 function getUrlBase(port) {
-    return "https://vdcszqvvrwdqcnjvcoxt.supabase.co/rest/v1/" + port + "select=*";
+    return "https://reirnbangazwplsopaju.supabase.co/rest/v1/" + port + "select=*";
 }
 
 function getTokenSupabase() {
-    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkY3N6cXZ2cndkcWNuanZjb3h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyNTUxNTUsImV4cCI6MTk3NTgzMTE1NX0.U-3HSFgKo9ydTnKrpQsx5ytrBcLSpGwzVn6LqNwn14E";
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlaXJuYmFuZ2F6d3Bsc29wYWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDEzMTIsImV4cCI6MTk3ODU3NzMxMn0.4rBwx9vOM48Q2IaV2vH20KPy-h6oRnRqURMv9WYp8cg";
 }
